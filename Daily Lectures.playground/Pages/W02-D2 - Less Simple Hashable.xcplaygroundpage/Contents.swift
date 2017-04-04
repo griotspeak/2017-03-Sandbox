80bcbdc5d2642ff85f04fc9fcba236f5f63f90f2
TJ Usiyan <griotspeak@gmail.com>
2017-04-04 15:17:49 -0400

    declaration order matters. sometimes.

--- Daily Lectures.playground/Pages/W02-D2 - Less Simple Hashable.xcplaygroundpage/Contents.swift
+++ Daily Lectures.playground/Pages/W02-D2 - Less Simple Hashable.xcplaygroundpage/Contents.swift
@@ -1,3 +1,19 @@
+func hashCombine(list: [Int]) -> Int {
+    var back: Int = 0
+
+    for i in list {
+        back = hashCombine(back, i)
+    }
+
+    return back
+}
+
+func hashCombine(_ lhs: Int, _ rhs: Int) -> Int {
+    let magicUInt64: UInt64 = 0x9e3779b97f4a7c15
+    let _magicHashValue = Int(truncatingBitPattern: magicUInt64)
+    return lhs ^ (rhs &+ _magicHashValue &+ (lhs << 6) &+ (lhs >> 2))
+}
+
 struct Location: Hashable {
     var x: Int
     var y: Int
@@ -22,18 +38,3 @@ let locations: Set<Location> = [
     Location(x: 0, y: 1)
 ]
 
-func hashCombine(list: [Int]) -> Int {
-    var back: Int = 0
-
-    for i in list {
-        back = hashCombine(back, i)
-    }
-
-    return back
-}
-
-func hashCombine(_ lhs: Int, _ rhs: Int) -> Int {
-    let magicUInt64: UInt64 = 0x9e3779b97f4a7c15
-    let _magicHashValue = Int(truncatingBitPattern: magicUInt64)
-    return lhs ^ (rhs &+ _magicHashValue &+ (lhs << 6) &+ (lhs >> 2))
-}
